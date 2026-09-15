79921422f87182665a5ff3307f0cfda8fd772da3
nicogaldo <nicogaldo@gmail.com>
2026-09-15 04:56:45 -0300

    release: prepare version 0.1.0-beta.3

--- src/JPutils/jp_version.h
+++ src/JPutils/jp_version.h
@@ -1,6 +1,6 @@
 #pragma once
 #ifndef GUIPPER_VERSION
-#define GUIPPER_VERSION "0.1.0-beta.2"
+#define GUIPPER_VERSION "0.1.0-beta.3"
 #endif
 #define GUIPPER_WIDEN_INNER(x) L##x
 #define GUIPPER_WIDEN(x) GUIPPER_WIDEN_INNER(x)
